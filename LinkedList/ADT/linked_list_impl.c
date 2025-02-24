be2275fad144382c8887ff9cb19e17be71283334
wild10086 <wild10086@10086.com>
2025-02-24 22:52:41 +0800

    Modify the search element logic

--- LinkedList/ADT/linked_list_impl.c
+++ LinkedList/ADT/linked_list_impl.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef __linux__
+    #include <sys/types.h>
+#endif
+
+
 #define INIT_DATA 0
+
 
 typedef int EleType;
 
 
-typedef struct LinkedNode {
+typedef struct LinkedListNode {
     EleType data;
-    struct LinkedNode* next; 
-} LinkedNode;
+    struct LinkedListNode* next; 
+} LinkedListNode;
 
 
 typedef struct LinkdeList {
-    LinkedNode* head;
+    LinkedListNode* head;
 
     size_t length;
 
-    ssize_t size; 
+    ssize_t size;
 } LinkdeList;
 
 
-LinkdeList* linkedlist_init(ssize_t size) {
+LinkdeList* linkedlist_create(ssize_t size) {
     LinkdeList* list = (LinkdeList*)malloc(sizeof(LinkdeList));
     if (list == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Failed to allocate memory for the LinkedList\n");
         return NULL;
     }
     list->length = 0;
@@ -51,17 +57,21 @@ int _linkedlist_is_exceed(LinkdeList* list) {
 
 
 int linkedlist_display(LinkdeList* list) {
-    if (list == NULL || list->length == 0) {
-        fprintf(stderr, "Exceed capacity limit\n");
+    if (list == NULL) {
+        fprintf(
+            stderr, 
+            "Parameter is incorrect. Please check whether the `LinkedList` type parameter is passed\n"
+        );
+
         return -1;
     }
-    LinkedNode* current = list->head;
+    LinkedListNode* current = list->head;
     while (current != NULL) {
         printf("%d  ", current->data);
         current = current->next;
     }
     puts("");
-    printf("length: %zu, max: %zd\n", list->length, list->size);
+    printf("length: %zu, max: %zd\n\n", list->length, list->size);
     return 0;
 }
 
@@ -72,9 +82,9 @@ int linkedlist_push_front(LinkdeList* list, EleType data) {
         return -1;
     }
 
-    LinkedNode* node = (LinkedNode*)malloc(sizeof(LinkedNode));
+    LinkedListNode* node = (LinkedListNode*)malloc(sizeof(LinkedListNode));
     if (node == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Failed to allocate memory for the LinkedListNode\n");
         return -1;
     }
 
@@ -95,17 +105,17 @@ int linkedlist_push_front(LinkdeList* list, EleType data) {
 
 int linkedlist_push_back(LinkdeList* list, EleType data) {
     if (_linkedlist_is_exceed(list) || list == NULL) {
-        fprintf(stderr, "Exceed capacity limit\n");
+        fprintf(stderr, "`LinkedList` Exceed capacity limit\n");
         return -1;
     }
 
-    LinkedNode* node = (LinkedNode*)malloc(sizeof(LinkedNode));
+    LinkedListNode* node = (LinkedListNode*)malloc(sizeof(LinkedListNode));
     if (node == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Failed to allocate memory for the `LinkedListNode`\n");
         return -1;
     }
     node->data = data;
-    LinkedNode* current = list->head;
+    LinkedListNode* current = list->head;
 
     if (list->length == 0) {
         node->next = NULL;
@@ -125,18 +135,18 @@ int linkedlist_push_back(LinkdeList* list, EleType data) {
 
 int linkedlist_insert(LinkdeList* list, EleType data, size_t pos) {
     if (_linkedlist_is_exceed(list) || list == NULL) {
-        fprintf(stderr, "Exceed capacity limit\n");
+        fprintf(stderr, "`LinkedList` Exceed capacity limit\n");
         return -1;
     }
 
     if (pos > list->length) {
-        fprintf(stderr, "Index overshoot\n");
+        fprintf(stderr, "`LinkedList` Index overshoot\n");
         return -1;
     } else {
-        LinkedNode* node = (LinkedNode*)malloc(sizeof(LinkedNode));
+        LinkedListNode* node = (LinkedListNode*)malloc(sizeof(LinkedListNode));
         node->data = data;
 
-        LinkedNode* current = list->head;
+        LinkedListNode* current = list->head;
         size_t count = 1;
 
         while ((current->next != NULL) && (count < pos - 1)) {
@@ -153,17 +163,21 @@ int linkedlist_insert(LinkdeList* list, EleType data, size_t pos) {
 
 
 int linkedlist_remove(LinkdeList* list, size_t pos) {
-    if (pos > list->length || list == NULL) {
-        fprintf(stderr, "Exceed capacity limit\n");
+    if (list == NULL) {
+        fprintf(
+            stderr, 
+            "Parameter is incorrect. Please check whether the `LinkedList` type parameter is passed\n"
+        );
+
         return -1;
     }
 
     if (pos > list->length) {
-        fprintf(stderr, "Index overshoot\n");
+        fprintf(stderr, "`LinkedList` Index overshoot\n");
         return -1;
     } else {
 
-        LinkedNode* current = list->head;
+        LinkedListNode* current = list->head;
         size_t count = 1;
 
         while ((current->next != NULL) && (count < pos - 1)) {
@@ -171,7 +185,7 @@ int linkedlist_remove(LinkdeList* list, size_t pos) {
             count++;
         }
 
-        LinkedNode* old = current->next;
+        LinkedListNode* old = current->next;
         current->next = current->next->next;
         free(old);
         list->length--;
@@ -180,37 +194,67 @@ int linkedlist_remove(LinkdeList* list, size_t pos) {
 }
 
 
-int linkedlist_search(LinkdeList* list, EleType data, size_t pos) {
+int linkedlist_search(LinkdeList* list, EleType** data, size_t** pos) {
     if (list == NULL) {
-        fprintf(stderr, "Exceed capacity limit\n");
+        fprintf(
+            stderr, 
+            "Parameter is incorrect. Please check whether the `LinkedList` type parameter is passed\n"
+        );
         return -1;
     }
 
-    if (pos > list->length) {
-        fprintf(stderr, "Index overshoot\n");
+    if (*pos != NULL && **pos > list->length) {
+        fprintf(stderr, "`LinkedList` Index overshoot\n");
         return -1;
-    } else {
+    }
+    else if (*pos != NULL && *data == NULL) {
 
-        LinkedNode* current = list->head;
+        LinkedListNode* current = list->head;
         size_t count = 1;
 
-        while ((current->next != NULL) && (count < pos)) {
+        while ((current != NULL) && (count < **pos)) {
+            current = current->next;
+            count++;
+        }
+        printf("%zu\n", count);
+        *data = (EleType*)malloc(sizeof(EleType));
+        **data = current->data;
+    }
+    else if (*pos == NULL && *data != NULL) {
+        LinkedListNode* current = list->head;
+        size_t count = 1;
+        int search_state = 0;
+
+        while (current != NULL) {
+            if (current->data == **data) {
+                search_state = 1;
+                *pos = (size_t*)malloc(sizeof(size_t));
+                **pos = count;
+                break;
+            }
             current = current->next;
             count++;
         }
 
-        data = current->data;
-        return 0;
+        if (search_state == 0) {
+            return -1;
+        }
+    } 
+    else {
+        fprintf(stderr, "`LinkedList` Search mode error\n");
+        return -1;
     }
+
+    return 0;
 }
 
 
 int linkedlist_pop_front(LinkdeList* list, EleType* data) {
     if (list == NULL || list->length == 0) {
-        fprintf(stderr, "Exceed capacity limit\n");
+        fprintf(stderr, "`LinkedList` Exceed capacity limit\n");
         return -1;
     }
-    LinkedNode* node = list->head;
+    LinkedListNode* node = list->head;
     *data = list->head->data;
     list->head = list->head->next;
     list->length--;
@@ -225,7 +269,7 @@ int linkedlist_pop_back(LinkdeList* list, EleType* data) {
         return -1;
     }
 
-    LinkedNode* current = list->head;
+    LinkedListNode* current = list->head;
     while (current->next->next != NULL) {
         current = current->next;
     }
@@ -254,10 +298,10 @@ int linkedlist_front(LinkdeList* list, EleType* data) {
 
 int linkedlist_back(LinkdeList* list, EleType* data) {
     if (list == NULL || list->length == 0) {
-        fprintf(stderr, "Exceed capacity limit\n");
+        fprintf(stderr, "`LinkedList` Exceed capacity limit\n");
         return -1;
     }
-    LinkedNode* current = list->head;
+    LinkedListNode* current = list->head;
 
     while (current->next != NULL) {
         current = current->next;
@@ -269,13 +313,13 @@ int linkedlist_back(LinkdeList* list, EleType* data) {
 
 int linkedlist_clean(LinkdeList* list) {
     if (list == NULL || list->length == 0) {
-        fprintf(stderr, "Exceed capacity limit\n");
+        fprintf(stderr, "`LinkedList` Exceed capacity limit\n");
         return -1;
     }
 
-    LinkedNode* current = list->head;
+    LinkedListNode* current = list->head;
     while (current->next != NULL) {
-        LinkedNode* old = current;
+        LinkedListNode* old = current;
         current = current->next;
         free(old);
     }
@@ -286,43 +330,73 @@ int linkedlist_clean(LinkdeList* list) {
 
 int main(int argc, char const *argv[]) {
     
-    LinkdeList* list = linkedlist_init(-1);
+    LinkdeList* list = linkedlist_create(-1);
 
-    linkedlist_push_back(list, 233);
+    puts("  push back: ");
     linkedlist_push_back(list, 2333);
     linkedlist_push_back(list, 23333);
+    linkedlist_push_back(list, 233333);
     linkedlist_display(list);
 
 
+    puts("  push front: ");
     linkedlist_push_front(list, 23);
     linkedlist_display(list);
 
 
-    linkedlist_insert(list, 73, 2);
+    puts("  insert 2: ");
+    linkedlist_insert(list, 233, 2);
     linkedlist_display(list);
 
-    linkedlist_remove(list, 2);
+
+    puts("  remove 5: ");
+    linkedlist_remove(list, 5);
     linkedlist_display(list);
+
 
     EleType ele;
+
+
+    puts("  pop front ");
     linkedlist_pop_front(list, &ele);
     linkedlist_display(list);
-    printf("%d\n", ele);
+    printf("popped element is: %d\n", ele);
 
+
+    puts("  pop back ");
     linkedlist_pop_back(list, &ele);
-    printf("%d\n", ele);
     linkedlist_display(list);
+    printf("popped element is:%d\n", ele);
 
+
+    puts("  lookup:");
+    printf("fornt element is: %d\n", ele);
     linkedlist_front(list, &ele);
-    printf("%d\n", ele);
     linkedlist_display(list);
 
-    linkedlist_search(list, &ele, 3);
-    printf("%d\n", ele);
-
+    printf("back element is: %d\n", ele);
     linkedlist_back(list, &ele);
-    printf("%d\n", ele);
     linkedlist_display(list);
+
+    puts("  search:");
+    size_t seq = 2;
+    size_t* pos = &seq;
+    EleType* res = NULL;
+    if (linkedlist_search(list, &res, &pos) == 0) {
+        printf("the 2 elemnet is: %d\n", *res);
+        free(res);
+    }
+    
+
+    size_t* target = NULL;
+    ele = 2333;
+    res = &ele;
+    if (linkedlist_search(list, &res, &target) == 0) {
+        printf("the 2333 elemnet pos is: %zu\n", *target);
+        free(target); // 确保释放内存
+    } else {
+        printf("Element 23333 not found.\n");
+    }
 
     linkedlist_clean(list);
 
