a6bb606fdbb2b2cc907f75b331bf3131902f39f0
wild10086 <wild10086@10086.com>
2025-02-25 17:21:10 +0800

    The Deque implements explicit queue semantics and is stored in heap area instead

--- Queue/ADT/deque_impl.c
+++ Queue/ADT/deque_impl.c
@@ -1,83 +1,112 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_LENGTH 500
+#ifdef __linux__
+    #include <sys/types.h>
+#endif
+
+
 #define INIT_DATA 0
 
 typedef int EleType;
 
 typedef struct Deque {
-    EleType elements[MAX_LENGTH];
-    int front;
-    int rear;
+    EleType* elements;
+    size_t max;
+    size_t front;
+    size_t rear;
 } Deque;
 
 
-void init(Deque *deque) {
-    for (int i = 0; i < MAX_LENGTH; i++)
+Deque* deque_create(size_t max) {
+    Deque* deque = (Deque*)malloc(sizeof(Deque));
+    if (deque == NULL) {
+        fprintf(stderr, "Failed to allocate memory for the `Deque`\n");
+        return NULL;
+    }
+
+    deque->elements = (EleType*)malloc(max * sizeof(EleType));
+    if (deque->elements == NULL) {
+        fprintf(stderr, "Failed to allocate memory for the `elements` of `Deque`\n");
+        free(deque);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < max; i++) {
         deque->elements[i] = INIT_DATA;
-    
+    }
+
+    deque->max = max;
     deque->front = 0;
     deque->rear = 0;
+
+    return deque;
 }
 
-bool is_empty(Deque deque) {
+
+int deque_is_empty(Deque deque) {
     return deque.front == deque.rear;
 }
 
-bool is_full(Deque *deque) {
-    return (deque->rear + 1) % MAX_LENGTH == deque->front;
+
+int deque_is_full(Deque *deque) {
+    return (deque->rear + 1) % deque->max == deque->front;
 }
 
 
-int len(Deque deque) {
-    return (deque.rear - deque.front + MAX_LENGTH) % MAX_LENGTH;
+ssize_t deque_length(Deque deque) {
+    return ((deque.rear + deque.max) - deque.front) % deque.max;
 }
 
 
-bool push_back(Deque *deque, EleType ele) {
-    if (is_full(deque))
-        return false;
-    
+int deque_enqueue(Deque *deque, EleType ele) {
+    if (deque_is_full(deque)) {
+        fprintf(stderr, "Queue is full, enqueue failed\n");
+        return -1;
+    }
+
     deque->elements[deque->rear] = ele;
-    deque->rear = (deque->rear + 1) % MAX_LENGTH;
-    return true;
+    deque->rear = (deque->rear + 1) % deque->max;
+    return 0;
 }
 
-bool push_front(Deque *deque, EleType ele) {
-    if (is_full(deque))
-        return false;
+
+int deque_dequeue(Deque *deque, EleType ele) {
+    if (deque_is_empty(*deque)) {
+        fprintf(stderr, "Queue is empty, queue failed\n");
+        return -1;
+    }
     
-    deque->front = (deque->front - 1 + MAX_LENGTH) % MAX_LENGTH;
+    deque->front = (deque->front - 1 + deque->max) % deque->max;
     deque->elements[deque->front] = ele;
-    return true;
+    return 0;
 }
 
 
-EleType pop_front(Deque *deque) {
-    if (is_empty(*deque))
-        return INIT_DATA;
-    
-    EleType _ele = deque->elements[deque->front];
-    deque->elements[deque->front] = INIT_DATA;
-    deque->front = (deque->front + 1) % MAX_LENGTH;
-    return _ele;
-}
-
-
-EleType pop_back(Deque *deque) {
-    if (is_empty(*deque))
-        return INIT_DATA;
-    
-    deque->rear = (deque->rear - 1 + MAX_LENGTH) % MAX_LENGTH;
-    EleType _ele = deque->elements[deque->rear];
-    deque->elements[deque->rear] = INIT_DATA;
-    return _ele;
+int deque_clean(Deque** deque) {
+    if (*deque == NULL) {
+        fprintf(
+            stderr, 
+            "Parameter is incorrect. Please check whether the `Deque*` type parameter is passed\n"
+        );
+        return -1;
+    }
+    free((*deque)->elements);
+    free(*deque);
+    *deque = NULL;
+    return 0;
 }
 
 
 int main(int argc, char const *argv[]) {
-    Deque deque;
-    init(&deque);
+    Deque* deque = deque_create(23);
+
+    printf("%p\n", deque);
+
+    deque_clean(&deque);
+
+    printf("%p\n", deque);
+    
     return 0;
 }
