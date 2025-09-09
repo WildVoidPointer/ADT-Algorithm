952b09456b741fa4751cf173653f66624db5d772
wp10086 <wp10086@wp10086.com>
2025-09-09 08:05:40 +0800

    new add th tree test

--- Tree/ThreadBinaryTree/thread_binary_tree_test.c
+++ Tree/ThreadBinaryTree/thread_binary_tree_test.c
@@ -7,29 +7,25 @@ int main() {
     int tree_node_array_len = 
         sizeof(tree_node_array) / sizeof(tree_node_array[0]);
 
+
+
     BinaryTree* bin_tree = BinaryTree_build_of_array(
         tree_node_array, tree_node_array_len
     );
-
     ThreadBinaryTree* built_th_tree = 
         ThreadBinaryTree_build_of_binary_tree(bin_tree);
 
     printf("ThreadBinaryTreeBuildResultOfBinaryTree\n");
-
     ThreadBinaryTree_display(
         built_th_tree, ThreadBinaryTree_pre_order_traverse
     );
-
     ThreadBinaryTree_display(
         built_th_tree, ThreadBinaryTree_in_order_traverse
     );
-
     ThreadBinaryTree_display(
         built_th_tree, ThreadBinaryTree_post_order_traverse
     );
-
     ThreadBinaryTree_clean(&built_th_tree);
-
     printf("ThreadBinaryTreeCleanResult: %p\n", built_th_tree);
     printf("====================\n");
 
@@ -37,17 +33,15 @@ int main() {
 
     ThreadBinaryTree* pre_order_thread_tree = 
         ThreadBinaryTree_build_of_binary_tree(bin_tree);
-    
-    printf("ThreadBinaryTreePreOrderThreadResult\n");
 
+    printf("ThreadBinaryTreePreOrderThreadResult\n");
     ThreadBinaryTree_threading(
         pre_order_thread_tree, ThreadBinaryTree_pre_order_traverse
     );
-
     ThreadBinaryTree_display(
         pre_order_thread_tree, ThreadBinaryTree_pre_order_threaded_traverse
     );
-    
+    ThreadBinaryTree_clean(&pre_order_thread_tree);
     printf("====================\n");
 
 
@@ -56,19 +50,22 @@ int main() {
         ThreadBinaryTree_build_of_binary_tree(bin_tree);
     
     printf("ThreadBinaryTreeInOrderThreadResult\n");
-
     ThreadBinaryTree_threading(
         in_order_thread_tree, ThreadBinaryTree_in_order_traverse
     );
-
     ThreadBinaryTree_display(
         in_order_thread_tree, ThreadBinaryTree_in_order_threaded_traverse
     );
+    ThreadBinaryTree_clean(&in_order_thread_tree);
+    printf("====================\n");
 
 
-    // ThreadBinaryTreeNode* threaded_root = 
-    //     ThreadBinaryTree_threaded_root(built_th_tree);
 
-    // ThreadBinaryTreeNode_display(threaded_root);
+    ThreadBinaryTree* post_order_thread_tree =
+        ThreadBinaryTree_build_of_binary_tree(bin_tree);
+    printf("ThreadBinaryTreePostOrderThreadResult\n");
+    ThreadBinaryTree_threading(
+        post_order_thread_tree, ThreadBinaryTree_post_order_traverse
+    );
     
-}
+}  
