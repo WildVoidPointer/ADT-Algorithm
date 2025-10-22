cd042a936921e8f3a8db5f597e606506da077f48
wp10086 <wp10086@wp10086.com>
2025-10-22 23:08:17 +0800

    Standardize the answering code for postgraduate entrance examination programming questions

--- Wangdao/LinearList/SequenceList/del_item_and_return.c
+++ Wangdao/LinearList/SequenceList/del_item_and_return.c
@@ -6,71 +6,79 @@
 /* -----------------------------以下为作答部分---------------------------------- */
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_LEN 100
-
-// 定义一个基准的无效值 增强顺序表的健壮性
-#define INVALID_VALUE 0
 
 typedef int SeqListDataType;
 
 // 定义一个顺序表结构，len 表示顺序表的实际元素个数
-// 且保证初始化后空表起始长度 len 为 0
 typedef struct SeqList {
-    int data[MAX_LEN];
+    SeqListDataType data[MAX_LEN];
     int len;
 } SeqList;
 
-SeqListDataType DeleteAndReturnMinElement(SeqList* sl) {
 
-    // 如果顺序表为空 打印错误信息
+// 因为当表为空时 随意返回可能会带来混淆 所以额外传递一个结果有效位
+SeqListDataType DeleteAndReturnMinElement(SeqList* sl, bool* flag) {
+
+    // 如果顺序表为空 打印错误信息 然后将 返回值有效位 置为 false
     if (sl->len <= 0) {
         printf("error: the SeqList is empty");
-        return -1;
+        *flag = false;
+        return 0;
     }
 
     // 选择非空顺序表的第一个元素作为基准
-    SeqListDataType min_item = sl->data[0];
+    SeqListDataType minValue = sl->data[0];
 
     // 记录最小元素在顺序表中数组的实际索引位置
-    int index = 0;
+    int minPos = 0;
 
-    // 从第二个元素开始按顺序查找最小值元素
+    // 如果表中元素不止一个 则从第二个元素开始按顺序查找最小值元素
     for (int i = 1; i < sl->len; i++) {
-        if (sl->data[i] < min_item) {
-            min_item = sl->data[i];
-            index = i;
+        if (sl->data[i] < minValue) {
+            minValue = sl->data[i];
+            minPos = i;
         }
     }
 
-    // 如果最小元素是最后一个 将其直接标记为无效值
-    // 将最后一个值赋值到最小元素的位置 并将最后一个元素标记为无效值
-    if (index == sl->len - 1) {
-        sl->data[index] = INVALID_VALUE;
-    } else {
-        sl->data[index] = sl->data[sl->len - 1];
-        sl->data[sl->len - 1] = INVALID_VALUE;
+    // 如果最小元素不是最后一个元素 将最后一个元素赋值给最小值元素位置
+    // 如果最小元素是最后一个元素 则直接返回
+    if (minPos != sl->len - 1) {
+        sl->data[minPos] = sl->data[sl->len - 1];
     }
 
     sl->len--;
 
-    // 返回最小值
-    return min_item;
+    // 将结果有效位置为 true 并返回最小值
+    *flag = true;
+    return minValue;
 }
 
 /* 
     算法实现如上，其中：
-    由于此处只进行了单重循环，且循环在最坏条件下的次数为 n ，
-    使用了两个个局部变量存储最小元素和最小元素的位置则
 
-    时间复杂度为 O(n)
-    空间复杂度为 O(1)
+    时间复杂度为 O(n) ------循环 n-1 次
 
-    若输入的线性表数据为：[1, 4, 5, 0, 6, 9]
-    则预期处理后的结果应为： [1, 4, 5, 6, 9]
+    空间复杂度为 O(1) ------使用了常数个辅助变量
+
+    算法思想：通过循环整个顺序表 记录并且操作最小元素位置
+
+    若输入的是一个空表 直接退出算法 并打印错误信息
+    输入顺序表：[]
+    预期输出：error: the SeqList is empty
+
+    若输入的是仅有一个元素的顺序表 不进入循环且直接修改顺序表长度后返回
+    输入顺序表：[ 1 ]
+    预期输出：1
+
+    若输入的线性表元素不止一个 则进入循环查找
+    输入顺序表：[ 1, 4, 5, 0, 6, 9 ]
+    预期处理后顺序表： [ 1, 4, 5, 6, 9 ]
+    预期输出为：0
 
  */
-
 
 /* -----------------------------作答部分结束---------------------------------- */
 
@@ -84,9 +92,11 @@ int main() {
         sl.len++;
     }
 
+    bool result_is_vaild = false;
+
     printf("SeqList len: %d , The last item: %d\n", sl.len, sl.data[sl.len - 1]);
 
-    printf("The min item: %d\n", DeleteAndReturnMinElement(&sl));
+    printf("The min item: %d\n", DeleteAndReturnMinElement(&sl, &result_is_vaild));
 
     printf("SeqList len: %d , The last item: %d\n", sl.len, sl.data[sl.len - 1]);
 
